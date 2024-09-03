ec9b5598d90ff260836587e3acf826d77343fac6
Daniyal Khan <d4n.kh4n@gmail.com>
2024-09-03 13:26:21 +0500

    identifires added

---
+++ Day 1/Identifires/identifires.js
@@ -0,0 +1,25 @@
+// Identifires 
+
+
+// Identifires: identifiers are used to name variables and functions and to provide labels for certain loops in JavaScript code. 
+//              A JavaScript identifier must begin with a letter, an underscore ( _ ), or a dollar sign ( $ ).
+
+// Characteristics:
+// Identifiers can consist of letters (both uppercase and lowercase), digits (0-9), underscores (_), and dollar signs ($) in some languages.
+// Identifiers cannot start with a digit.
+// They are case-sensitive in most programming languages (myVar and myvar would be different identifiers).
+// Reserved keywords (like for, if, while, etc.) cannot be used as identifiers.
+
+
+// Relationship Between Identifiers and Variables
+// An identifier is the name you give to a variable. Think of the identifier as a label, while the variable is the storage space labeled by the identifier.
+// In the context of variables, an identifier is used to refer to the variable's memory location where data is stored.
+
+
+
+let age = 25; // 'age' is the identifier, 25 is the value, and 'let' is the keyword used to declare the variable.
+const MAX_HEIGHT = 180; // 'MAX_HEIGHT' is the identifier, 180 is the value, and 'const' is the keyword.
+
+
+// Identifiers: age, MAX_HEIGHT
+// Variables: The storage locations referred to by age and MAX_HEIGHT that hold the values 25 and 180, respectivel
