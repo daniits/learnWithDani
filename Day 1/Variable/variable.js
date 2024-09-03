9199ea2dee0e2cc72cde80fdf5e60cd26f0fabda
Daniyal Khan <d4n.kh4n@gmail.com>
2024-09-03 12:28:28 +0500

    Variable Var has been added

--- Day 1/Variable/variable.js
+++ Day 1/Variable/variable.js
@@ -7,8 +7,8 @@
 //Function Scope:  Variables declared inside a function are only accessible within that function. They are not accessible outside of the function in which they are defined.
 //Example 
 function myFunction() {
-    var functionScopedVariable = "I am a function-scoped variable"; // function scope
-    console.log(functionScopedVariable); // Output: I am a function-scoped variable
+  var functionScopedVariable = "I am a function-scoped variable"; // function scope
+  console.log(functionScopedVariable); // Output: I am a function-scoped variable
 }
 
 myFunction();
@@ -21,7 +21,7 @@ myFunction();
 var globalVariable = "I am a global variable"; // global scope
 
 function greet() {
-    console.log(globalVariable); // Accessible inside the function
+  console.log(globalVariable); // Accessible inside the function
 }
 
 greet(); // Output: I am a global variable
@@ -32,34 +32,43 @@ console.log(globalVariable); // Output: I am a global variable
 //Blocked Scope:  Variables declared inside a block (e.g., within curly braces {}) are only accessible within that block. This scope applies to variables declared with let or const.
 //Example
 if (true) {
-    let blockScopedVariable = "I am a block-scoped variable"; // block scope
-    console.log(blockScopedVariable); // Output: I am a block-scoped variable
-  }
-  
-  // console.log(blockScopedVariable); // Uncaught ReferenceError: blockScopedVariable is not defined
+  let blockScopedVariable = "I am a block-scoped variable"; // block scope
+  console.log(blockScopedVariable); // Output: I am a block-scoped variable
+}
+
+// console.log(blockScopedVariable); // Uncaught ReferenceError: blockScopedVariable is not defined
 
 
 // Variable
 // A Variable is Container for storing data values. It allows you to save data that can be used and manipulated later in your program.
 // Variable decleared using the keyword:
-
-// Var: Variable are decleared with var are function scope or global scope
+// var
+// Scope: Variable are decleared with var are function scope or global scope
+// Hoisting: Variables declared with var are hoisted to the top of their scope and initialized with undefined.
+// Re-declaration: You can re-declare a var variable within the same scope without any error.
 
 function testVar() {
-    var x = 10;
-    if (true) {
-      var x = 20; // Same variable, scope is function-wide
-      console.log(x); // Output: 20
-    }
-    console.log(x); // Output: 20 (still 20, because 'var' is function-scoped)
+  var x = 10;
+  if (true) {
+    var x = 20; // Same variable, scope is function-wide
+    console.log(x); // Output: 20
   }
-  
-  testVar();
-  
-  // Global scope example
-  var y = 30;
-  console.log(y); // Output: 30
-  
-  var y = 40; // No error, 'y' is re-declared
-  console.log(y); // Output: 40
-  
+  console.log(x); // Output: 20 (still 20, because 'var' is function-scoped)
+}
+
+testVar();
+
+// Global scope example
+var y = 30;
+console.log(y); // Output: 30
+
+// Hoisting with var
+
+console.log(y) // Output: undefined (hoisted, but not initialized)
+var y = 5;
+console.log(y) // Output: 5
+
+
+
+// Let 
+// Scope: let is block-scoped, meaning it is only accessible within the block (e.g., within {}) it was declared in.
