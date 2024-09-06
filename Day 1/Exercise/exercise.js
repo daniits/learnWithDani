f90242f10b142c1b29e83b74701d7d5ad4ca0969
unknown <d4n.kh4n@gmail.com>
2024-09-07 02:57:40 +0500

    intializer and identifires added

---
+++ Day 1/Exercise/exercise.js
@@ -0,0 +1,13 @@
+// Solve These Problem 
+
+// What Was Output of this Code?
+function checkAge(age) {
+    if (age > 18) {
+        const message = "hello"
+    }
+    else {
+        const message = "hello world"
+    }
+    return message
+}
+console.log(checkAge(20))
---
+++ Day 1/IntializerVsIdentifires/ivsd.js
@@ -0,0 +1,11 @@
+// What is Intializer? 
+//  The initializer assigns an initial value to a variable when it's declared.
+// Example 
+let age = 25;  // '25' is the initializer.
+const name = "John";  // "John" is the initializer.
+
+
+// What is Decelration?
+// A declaration defines the type or nature of the variable or object but does not always assign a value.
+let value;  // Declaration of the variable 'value' without assigning a value.
+// const valuename;  // Declaration of the constant 'valuename' (this will throw an error because constants require initialization at the time of declaration).
