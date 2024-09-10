26dccaef868fd6cfd25c26a3c3136d9db823cbdd
Daniyal Khan <d4n.kh4n@gmail.com>
2024-09-10 13:16:11 +0500

    data types added

--- Courses/Days/Day 2/dtandds.js
+++ Courses/Days/Day 2/dtandds.js
@@ -1,2 +1,47 @@
+// Data Types & Data Structure
+// Programming languages all have built-in data structures, but these often differ from one language to another.
 
 // What is Data Type? 
+// In JavaScript, data types and data structures play a fundamental role in how information is stored, accessed, and manipulated. 
+// JavaScript Has Two Main Data Type: 
+
+// Primitive Data Types 
+// These are MOst Basic Data Type 
+
+// Number: Represents numeric values (both integers and floating-point numbers).
+let age = 25; // 25 is Number 
+let pi = 3.14159; // 3.14159 is also data type is number
+
+// String: Represents a sequence of characters (text).
+let name = "John Doe"; // John Doe is String
+
+// Boolean: Represents logical values, either true or false.
+let isJavaScriptFun = true; // It will be True or False Values
+
+// Null: Represents the intentional absence of a value. It is treated as an object but is a primitive.
+let noValue = null;
+
+// Undefined: Indicates a variable that has been declared but not assigned a value.
+let undefinedVariable;
+
+// Symbol: Introduced in ECMAScript 6 (ES6), it represents a unique identifier, often used for object properties.
+let symbol1 = Symbol('unique');
+
+// BigInt: Introduced in ES2020, it is used for working with large integers beyond the safe limit of Number.
+let bigNumber = BigInt(9007199254740991);
+
+
+// Non Premative Data Type
+// These are mutable and can hold collections of data or more complex entities.
+
+// Object: A collection of key-value pairs, where keys are strings or symbols and values can be any data type.
+let person = { name: "John", age: 30 };
+
+// Array: A special type of object used to store ordered collections of items (elements).
+let fruits = ["apple", "banana", "cherry"];
+
+// Function: A reusable block of code that can be called with arguments to perform a task.
+function greet() { return "Hello!"; }
+
+
+
